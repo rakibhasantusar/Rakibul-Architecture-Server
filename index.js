152546db302acbeb2ff375c5199e77b680924226
Rakibul Hasan <39860864+rakibhasantusar@users.noreply.github.com>
2022-11-08 01:15:31 +0600

    server ready env file, git ignore done

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config()
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middle ware
 app.use(cors());
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.RAKIBUL_USER}:${process.env.RAKIBUL_PASS}@cluster0.79y2pqi.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
 
 app.get("/", (req, res) => {
     res.send("Rakibul Architecture server is running");
